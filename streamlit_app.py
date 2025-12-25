fdb08ad33c3f7452bb1eb5c807bfa9282e0c8ec8
yantaiqz <yantaiqz@googlemail.com>
2025-12-25 22:45:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -385,7 +385,7 @@ with st.expander(get_txt('stat_tab_title'), expanded=True):
 # ==========================================
 # 9. 咖啡打赏 (和省份数据一样的 Expander 下拉/隐藏效果)
 # ==========================================
-st.markdown("---")
+#st.markdown("---")
 with st.expander(get_txt('coffee_title'), expanded=False):  # 默认收起
     # 打赏成功提示
     if st.session_state.donate_success:
